acd72bc336de3b7f95aef97ca8e1c590509a60c0
timdan-3 <timdan-3@student.ltu.se>
2016-03-08 15:08:33 +0100

    changed server response

--- quic_spdy_server_stream.cc
+++ quic_spdy_server_stream.cc
@@ -29,7 +29,9 @@ namespace tools {
 
 QuicSpdyServerStream::QuicSpdyServerStream(QuicStreamId id,
                                            QuicSpdySession* session)
-    : QuicSpdyStream(id, session), content_length_(-1) {}
+    : QuicSpdyStream(id, session), content_length_(-1) {
+//	this->lb = lb;
+}
 
 QuicSpdyServerStream::~QuicSpdyServerStream() {
 }
@@ -45,13 +47,13 @@ void QuicSpdyServerStream::OnDataAvailable() {
     }
     body_.append(static_cast<char*>(iov.iov_base), iov.iov_len);
 
-    if (content_length_ >= 0 &&
-        static_cast<int>(body_.size()) > content_length_) {
-      DVLOG(1) << "Body size (" << body_.size() << ") > content length ("
-               << content_length_ << ").";
-//      SendErrorResponse();
-      return;
-    }
+//    if (content_length_ >= 0 &&
+//        static_cast<int>(body_.size()) > content_length_) {
+//      DVLOG(1) << "Body size (" << body_.size() << ") > content length ("
+//               << content_length_ << ").";
+//      cout << "fail server " << endl;
+//      return;
+//    }
     MarkConsumed(iov.iov_len);
   }
   if (!sequencer()->IsClosed()) {
@@ -61,168 +63,53 @@ void QuicSpdyServerStream::OnDataAvailable() {
 
   // If the sequencer is closed, then all the body, including the fin, has been
   // consumed.
+
+
+
+
+
+  cout << "Server Received ";
+  printf("%.*s", 4, body_.c_str());
+
+  cout << " with size " << body_.length() << endl;
+
   OnFinRead();
 
   if (write_side_closed() || fin_buffered()) {
     return;
   }
 
-  cout << "Server Received: " << body_ << endl;
-  SendEcho(body_);
+  char test[4];
+  std::string str = body_.c_str();
+  str.copy(test, 4);
+
+
+  SendEcho(test);
   return;
 }
 
-void QuicSpdyServerStream::SendEcho(StringPiece data){
+void QuicSpdyServerStream::SendEcho(string data){
+
+	WriteOrBufferData("ACK: ", false, nullptr);
+	WriteOrBufferData(data, true, nullptr);
+	return;
+
+//	lb->WriteToIPERF(data);
+//	WriteOrBufferData(lb->ReadFromIPERF(), true, nullptr);
+//
+//	return;
 
 	 if (!reading_stopped()) {
 	    StopReading();
 	  }
 
-	  fake_WriteHeaders(false);
+//	  fake_WriteHeaders(false);
 
 	  if (!data.empty()) {
 		  WriteOrBufferData("ECHO: ", false, nullptr);
 		  WriteOrBufferData(data, true, nullptr);
 	  }
 }
-
-
-//void QuicSpdyServerStream::OnStreamHeadersComplete(bool fin, size_t frame_len) {
-//  QuicSpdyStream::OnStreamHeadersComplete(fin, frame_len);
-////  if (!ParseRequestHeaders(decompressed_headers().data(),
-////                           decompressed_headers().length())) {
-////    DVLOG(1) << "Invalid headers";
-//////    SendErrorResponse();
-////  }
-//  MarkHeadersConsumed(decompressed_headers().length());
-//}
-
-
-//bool QuicSpdyServerStream::ParseRequestHeaders(const char* data,
-//                                               uint32 data_len) {
-//  DCHECK(headers_decompressed());
-//  SpdyFramer framer(HTTP2);
-//  if (!framer.ParseHeaderBlockInBuffer(data, data_len, &request_headers_) ||
-//      request_headers_.empty()) {
-//    return false;  // Headers were invalid.
-//  }
-//
-//  if (ContainsKey(request_headers_, "content-length")) {
-//    string delimiter;
-//    delimiter.push_back('\0');
-//    std::vector<string> values =
-//        base::SplitString(request_headers_["content-length"], delimiter,
-//                          base::KEEP_WHITESPACE, base::SPLIT_WANT_ALL);
-//
-//    for (const string& value : values) {
-//      int new_value;
-//      if (!StringToInt(value, &new_value) || new_value < 0) {
-//        return false;
-//      }
-//      if (content_length_ < 0) {
-//        content_length_ = new_value;
-//        continue;
-//      }
-//      if (new_value != content_length_) {
-//        return false;
-//      }
-//    }
-//  }
-//
-//  return true;
-//}
-
-
-
-//void QuicSpdyServerStream::SendResponse() {
-//
-//  if (!ContainsKey(request_headers_, ":authority") ||
-//      !ContainsKey(request_headers_, ":path")) {
-//    DVLOG(1) << "Request headers do not contain :authority or :path.";
-//    SendErrorResponse();
-//    return;
-//  }
-//
-//  // Find response in cache. If not found, send error response.
-//  const QuicInMemoryCache::Response* response =
-//      QuicInMemoryCache::GetInstance()->GetResponse(
-//          request_headers_[":authority"], request_headers_[":path"]);
-//  if (response == nullptr) {
-//    DVLOG(1) << "Response not found in cache.";
-//    SendErrorResponse();
-//    return;
-//  }
-//
-//  if (response->response_type() == QuicInMemoryCache::CLOSE_CONNECTION) {
-//    DVLOG(1) << "Special response: closing connection.";
-//    CloseConnection(QUIC_NO_ERROR);
-//    return;
-//  }
-//
-//  if (response->response_type() == QuicInMemoryCache::IGNORE_REQUEST) {
-//    DVLOG(1) << "Special response: ignoring request.";
-//    return;
-//  }
-//
-//  // Examing response status, if it was not pure integer as typical h2 response
-//  // status, send error response.
-//  string request_url = request_headers_[":scheme"].as_string() + "://" +
-//                       request_headers_[":authority"].as_string() +
-//                       request_headers_[":path"].as_string();
-//  int response_code;
-//  SpdyHeaderBlock response_headers = response->headers();
-//  if (!base::StringToInt(response_headers[":status"], &response_code)) {
-//    DVLOG(1) << "Illegal (non-integer) response :status from cache: "
-//             << response_headers[":status"].as_string() << " for request "
-//             << request_url;
-//    SendErrorResponse();
-//    return;
-//  }
-//
-//  if (id() % 2 == 0) {
-//    // A server initiated stream is only used for a server push response,
-//    // and only 200 and 30X response codes are supported for server push.
-//    // This behavior mirrors the HTTP/2 implementation.
-//    bool is_redirection = response_code / 100 == 3;
-//    if (response_code != 200 && !is_redirection) {
-//      LOG(WARNING) << "Response to server push request " << request_url
-//                   << " result in response code " << response_code;
-//      Reset(QUIC_STREAM_CANCELLED);
-//      return;
-//    }
-//  }
-//  DVLOG(1) << "Sending response for stream " << id();
-////  SendHeadersAndBody(response->headers(), response->body());
-//}
-
-//void QuicSpdyServerStream::SendErrorResponse() {
-////  DVLOG(1) << "Sending error response for stream " << id();
-////  SpdyHeaderBlock headers;
-////  headers[":status"] = "500";
-////  headers["content-length"] = base::UintToString(strlen(kErrorResponseBody));
-////  SendHeadersAndBody(headers, kErrorResponseBody);
-//}
-
-//void QuicSpdyServerStream::SendHeadersAndBody(
-//    const SpdyHeaderBlock& response_headers,
-//    StringPiece body) {
-//  // This server only supports SPDY and HTTP, and neither handles bidirectional
-//  // streaming.
-//  if (!reading_stopped()) {
-//    StopReading();
-//  }
-//
-////  WriteHeaders(response_headers, body.empty(), nullptr);
-//  fake_WriteHeaders(false);
-//
-//  if (!body.empty()) {
-//	  cout << "sending response from server" << endl;
-//	  WriteOrBufferData("hello from server", true, nullptr);
-//    //WriteOrBufferData(body, true, nullptr);
-//  }
-//}
-
-//const char* const QuicSpdyServerStream::kErrorResponseBody = "bad";
 
 }  // namespace tools
 }  // namespace net
